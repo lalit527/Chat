a2047a90a8e1e8b841c631ce08aa91b3f5c411d7
lalit-07 <lalit.slg007@gmail.com>
2017-08-15 23:46:01 +0530

    Initial Copy

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('user',function(data){
+    console.log(data+ "came online");
+    socket.broadcast.emit('chat message', data+" came online");
+    // you can allocate variables in socket.
+    socket.user = data;
+
+
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', socket.user+' : '+msg);
+
+  });
+
+  socket.on('disconnect',function(){
+  	 
+  	 console.log("some user left the chat");
+     socket.broadcast.emit('chat message', socket.user+" left the chat");
+  }); //end socket disconnected
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
